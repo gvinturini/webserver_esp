8da76d85443ea5104773318d6f95457ddb4624ad
Giovanni Vinturini <go_vinturini@hotmail.com>
2019-03-04 18:45:17 -0300

    Webserver esp32 - método POST e conversão dos dados recebidos para char.

---
+++ webserver_esp32.ino
@@ -0,0 +1,51 @@
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+
+const char* ssid = "Giovini";
+const char* password = "navi2517";
+
+char dados[] = "";
+size_t tam;
+
+AsyncWebServer server(80);
+
+void array_to_string(byte array[], unsigned int len, char buffer[]);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.println("Conectando ao WiFi...");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  server.on(
+    "/post",
+    HTTP_POST,
+    [](AsyncWebServerRequest * request){},
+    NULL,
+    [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+      
+      for (size_t i = 0; i < len; i++) {
+        Serial.write(data[i]);
+        dados[i] = (char)data[i];        
+      }
+      tam = len;
+      Serial.println();
+      //Serial.println(dados);
+ 
+      request->send(200);
+  });
+ 
+  server.begin();
+}
+
+void loop() {
+  delay(2000);
+  Serial.println(tam);
+  Serial.println(dados);
+}
